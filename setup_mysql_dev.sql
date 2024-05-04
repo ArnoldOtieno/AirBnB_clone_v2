e41946536e0604867659bce73cd07d015450a603
ArnolOtieno <arnoldotieno31@gmail.com>
2024-05-04 08:55:07 -0700

    preparing sql server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,6 @@
 -- Script that prepares MYSQL server for project
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE OR REPLACE USER `hbnb_dev`@`localhost` IDENTIFIED BY `hbnb_test_pwd`;
-GRANT USAGE ON *.* TO `hbnb_dev`@`localhost`
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY `hbnb_test_pwd`;
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO `hbnb_dev`@`localhost`;
 FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO `hbnb_dev`@`localhost`;
