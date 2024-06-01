9779c71973ddd90f7e4a0f5f5ebd11356e6c706b
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-06-01 16:28:01 +0000

    displaying formatted text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Starting a Flask app.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+
+@app.route("/")
+def home():
+    """returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    text_no_underscore = text.replace('_', ' ')
+    return "C {}".format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
